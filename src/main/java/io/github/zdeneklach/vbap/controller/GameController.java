f6194a2e0869e37b4469ef27ef097b032c4b9cd9
Sidonius <lach.zdenek@gmail.com>
2022-05-15 00:58:38 +0200

    Added @CrossOrigin

--- src/main/java/io/github/zdeneklach/vbap/controller/GameController.java
+++ src/main/java/io/github/zdeneklach/vbap/controller/GameController.java
@@ -6,12 +6,13 @@ import io.github.zdeneklach.vbap.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/games")
 public class GameController {
--- src/main/java/io/github/zdeneklach/vbap/controller/GameRoomController.java
+++ src/main/java/io/github/zdeneklach/vbap/controller/GameRoomController.java
@@ -6,14 +6,16 @@ import io.github.zdeneklach.vbap.service.GameRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/gamerooms")
+
 public class GameRoomController {
     @Autowired
     GameRoomService gameRoomService;
